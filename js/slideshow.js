871cf991f5324b85a69c3c1f977d521c698e224f
Matt Allen <sdmix@users.noreply.github.com>
2019-06-17 22:05:47 +0100

    home updates, buttons, content, fonts

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,7 +6,7 @@ var makeBSS = function (a, b) {
                 this.counter = 0, this.el = a, this.$items = a.querySelectorAll("figure"), this.numItems = this.$items.length, b = b || {}, b.auto = b.auto || !1, this.opts = {
                     auto: "undefined" == typeof b.auto ? !1 : b.auto,
                     speed: "undefined" == typeof b.auto.speed ? 4000 : b.auto.speed
-                }, this.$items[0].classList.add("bss-show"), this.opts.auto && this.autoCycle(this.el, this.opts.speed, this.opts.pauseOnHover), this.opts.fullScreen && this.addFullScreen(this.el), this.opts.swipe && this.addSwipe(this.el)
+                }, this.$items[0].classList.add("bss-show"), this.opts.auto && this.autoCycle(this.el, this.opts.speed)
             },
             showCurrent: function (a) {
                 this.counter = a > 0 ? this.counter + 1 === this.numItems ? 0 : this.counter + 1 : this.counter - 1 < 0 ? this.numItems - 1 : this.counter - 1, [].forEach.call(this.$items, function (a) {
